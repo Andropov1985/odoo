5e2873c2e5f4f6ef99692d6fc4f941844e1d56cf
Andropov1985 <achille.sayom@gmail.com>
2021-09-21 14:20:05 +0000

    [ADD] Advanced ORM

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -17,3 +17,24 @@ class Course(models.Model):
                             copy=False)
     
     active = fields.Boolean(string='Active',default=True)
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    
+    additional_fee = fields.Float(string='Additional Fee', default=10.0)
+    
+    total_price = fields.Float('Total Price', readonly=True)
+    
+    @api.onchange('base_price','additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.0:
+            raise UserError('Base cannot be set as negative.')
+            
+            
+        self.total_price = self.base_price + self.additional_fee
+        
+    @api.constrains('additional_fee')
+    def _chek_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional Fees cannot be less than 10.00: %s' % record.additional_fee)
+                
